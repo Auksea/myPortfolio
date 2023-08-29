928ee85f0ee22af30ba3fa9c82576d4cfacab9f6
Auksea <66auksyte@gmail.com>
2023-08-29 13:31:09 +0200

    .

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -4,7 +4,7 @@ export const ProjectData=[
         title:"OcutuneApp",
         about: " Creation of database platform (in Azure) to store and extract data from Sensor and Nano Lambda Application. ",
         tags:["ReactJS", "Application Web Design", "Web Development"],
-        image: "C:\Users\Aukse\Desktop\Portfolio\React-Portfolio\src\assets\OcutuneApp.JPG",
+        image: "",
         github:"https://github.com/Auksea/OcutuneApp",
     },
     {
@@ -12,7 +12,7 @@ export const ProjectData=[
         title:"PWA and Wordpress",
         about: "I created a web application which after was wrapped in PWA and now is being used as an application in users devices. ",
         tags:["WordPress", "UX/UI", "PWA"],
-        image:"src\assets\Picture1.jpg",
+        image:"",
     },
     {
         id:1,
@@ -20,7 +20,7 @@ export const ProjectData=[
         about: "  Full stack web application. Climate Right is a personalised ventilation system designed to maximase personal comfort in a controlled indoor climate environment. ",
         tags:["Angular", "Ionic", "mySQL", "ASP.NET Core"],
         github:"https://github.com/RaniaZarka/Climate_Right",
-        image: "src/assets/ClimateRight.JPG",
+        image: "",
     },
     {
         id:1,
@@ -28,6 +28,6 @@ export const ProjectData=[
         about: " The idea of the web application is to have a device that is installed in the asthma patients room, that checks the humidity level in the room. The patient can set an alert if they want, so they will be notified when the humidity level is higher or lower that values they have defined. The webpage will also display the temperature and humidity outside. ",
         tags:["C#", "HTML", "", "TypeScript" , "CSS", "SQL", "Asp.Net Core", "XP programming"],
         github:"https://github.com/Auksea/humidicWebService",
-        image:"src\assets\ClimateRight.JPG",
+        image:"",
     },
 ];
